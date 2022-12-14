120f13b2bcfb8e35e3878830b6b3105549e39ea5
Marcelo Henrique <mhbcoelho99@gmail.com>
2022-12-14 10:50:35 -0300

    Verificação-de-cadastro

--- src/components/form/FormRegister.js
+++ src/components/form/FormRegister.js
@@ -1,4 +1,4 @@
-import React from 'react'
+import React, { useState } from 'react'
 import Button from './Button'
 import bikeStoreLogoIcon from '../../icons/bikestoreLogoIcon.svg'
 import Input from './Input'
@@ -6,14 +6,80 @@ import { NavLink } from 'react-router-dom'
 import styles from '../../styles/form/FormRegister.module.css'
 
 const FormRegister = () => {
+  const [error, setError] = useState(false)
+  const [username, setUsername] = useState('')
+  const [email, setEmail] = useState('')
+  const [password, setPassword] = useState('')
+  const [verifyPassword, setVerifyPassword] = useState('')
+
+
+  // Verifica Tamanho da senha
+  function handleBlur(e) {
+    if(password.length < 12) {
+      setError(true)
+    } else {
+      setError(false)
+    }
+
+    if(verifyPassword !== password) {
+      setError(true)
+    } else {
+      setError(false)
+    }
+
+  }
+
+  function handleSubmit(e) {
+    e.preventDefault();
+    if(password !== verifyPassword) {
+      console.log('As senhas precisam ser iguais')
+      setError(true)
+    } else {
+      setError(false)
+    }
+
+    if(password === verifyPassword) {
+      console.log('conta criada com sucesso')
+    }
+  }
+
+
   return (
-    <form className={styles.form}>
+    <form onSubmit={handleSubmit} className={styles.form}>
         <img src={bikeStoreLogoIcon} alt="BikeStore Icone"/>
         <h1>Sign In</h1>
-        <Input type="text" placeholder="Digite seu nome" required/>
-        <Input type="email" placeholder="aaron@column.com" required/>
-        <Input type="password" placeholder="Digite sua senha" required/>
-        <Input type="password" placeholder="Confime sua senha" required/>  
+
+        <Input
+        type="text"
+        value={username}
+        onChange={(event) => setUsername(event.target.value)}
+        placeholder="Digite seu nome" 
+        required/>
+
+        <Input 
+        type="email" 
+        value={email}
+        onChange={(event) => setEmail(event.target.value)}
+        placeholder="aaron@column.com" 
+        required/>
+
+        <Input 
+        type="password" 
+        value={password}
+        onChange={(e)=> setPassword(e.target.value)}
+        onBlur={handleBlur}
+        placeholder="Digite sua senha" 
+        required/>
+        {error && password.length < 12 && <p>A senha precisa conter no mínimo 12 caracteres</p>}
+        
+        <Input 
+        type="password" 
+        value={verifyPassword}
+        onChange={(event) => setVerifyPassword(event.target.value)}
+        placeholder="Confime sua senha" 
+        required/>  
+        {error && password !== verifyPassword && password.length >= 12 && <p>As senhas não coincidem</p>}
+
         <Button>Sign in</Button>
         <NavLink className={styles.haveAnAccount} to='/login'>Already have an account?</NavLink>
     </form>
