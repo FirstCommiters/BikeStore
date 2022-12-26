2e21dd53b94b696484c317d9fbe54c8feb5fddc9
Marcelo Henrique <mhbcoelho99@gmail.com>
2022-12-26 11:49:46 -0300

    página de bicicletas

--- src/pages/Bicicletas.js
+++ src/pages/Bicicletas.js
@@ -16,6 +16,7 @@ const Bicicletas = () => {
     <>
       <Intro text="Escolha a melhor para você" title="nossas bicicletas"/>
         <main className={styles.bicicletasBg}>
+            
             <div className={styles.bicicleta}>
                 <div className={styles.bicicletaImagem}>
                     <img src={Nimbus} alt="nimbus"/>
@@ -55,9 +56,88 @@ const Bicicletas = () => {
                     </div>
                     
                 </div>
-
-
             </div>
+
+            <div className={styles.bicicleta}>
+                <div className={styles.bicicletaImagem}>
+                    <img src={Magic} alt="nimbus"/>
+                    <span>R$ 4999</span>
+                </div>
+                <div className={styles.bicicletaConteudo}>
+                    <h2>Nimbus Stark</h2>
+                    <p>A Nimbus Stark é a melhor Bikcraft já criada pela nossa equipe. 
+                        Ela vem equipada com os melhores acessórios, o que garante maior 
+                        velocidade.
+                    </p>
+
+                    <div className={styles.informacoes}>
+
+                        <ul>
+                            <li>
+                                <img className={styles.stick} src={Motor} alt=""/>
+                                Motor Elétrico
+                            </li>
+
+                            <li>
+                                <img className={styles.stick} src={Fibra} alt=""/>
+                                Fibra de Carbono
+                            </li>
+
+                            <li>
+                                <img className={styles.stick} src={Velocidade} alt=""/>
+                                50km/h
+                            </li>
+
+                            <li>
+                                <img className={styles.stick} src={Rastreador} alt=""/>
+                                Rastreador
+                            </li>
+                        </ul>
+                        <ButtonCta>Mais sobre</ButtonCta>
+                    </div>
+                    
+                </div>
+            </div>
+
+            <div className={styles.bicicleta}>
+                <div className={styles.bicicletaImagem}>
+                    <img src={Nebula} alt="nimbus"/>
+                    <span>R$ 4999</span>
+                </div>
+                <div className={styles.bicicletaConteudo}>
+                    <h2>Nimbus Stark</h2>
+                    <p>A Nimbus Stark é a melhor Bikcraft já criada pela nossa equipe. 
+                        Ela vem equipada com os melhores acessórios, o que garante maior 
+                        velocidade.
+                    </p>
+
+                    <div className={styles.informacoes}>
+                        <ul>
+                            <li>
+                                <img className={styles.stick} src={Motor} alt=""/>
+                                Motor Elétrico
+                            </li>
+
+                            <li>
+                                <img className={styles.stick} src={Fibra} alt=""/>
+                                Fibra de Carbono
+                            </li>
+
+                            <li>
+                                <img className={styles.stick} src={Velocidade} alt=""/>
+                                50km/h
+                            </li>
+
+                            <li>
+                                <img className={styles.stick} src={Rastreador} alt=""/>
+                                Rastreador
+                            </li>
+                        </ul>
+                        <ButtonCta>Mais sobre</ButtonCta>
+                    </div>      
+                </div>
+            </div>
+            <span className={styles.pages}>1 2 3 4</span>
         </main>
     </>
   )
